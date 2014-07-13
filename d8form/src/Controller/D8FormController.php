ea5c0978d818663e3150dadbd97d00c20ae40915
Nguyen Tien Si <info@nguyentiensi.com>
2014-07-13 10:17:57 +0700

    Training - day 2

--- d8form/src/Controller/D8FormController.php
+++ d8form/src/Controller/D8FormController.php
@@ -1,9 +1,9 @@
 <?php
- 
+
 namespace Drupal\d8form\Controller;
- 
+
 use Drupal\Core\Controller\ControllerBase;
- 
+
 class D8FormController extends ControllerBase {
 
   /**
@@ -12,5 +12,13 @@ class D8FormController extends ControllerBase {
   public function d8formPage() {
     return \Drupal::formBuilder()->getForm('Drupal\d8form\Form\D8Form');
   }
- 
+
+  public function d8formPageTitle() {
+    return $this->t('Drupal 8 Form Page');
+  }
+
+  public function d8formTitle() {
+    return $this->t('Drupal 8 Form');
+  }
+
 }
