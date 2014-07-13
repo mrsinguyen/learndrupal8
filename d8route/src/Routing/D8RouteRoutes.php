af8e2df2168e2a43109cdb3858cab13c46e198fa
Nguyen Tien Si <info@nguyentiensi.com>
2014-07-13 13:32:42 +0700

    Training - Day 1 - Added route alter

--- d8route/src/Routing/D8RouteRoutes.php
+++ d8route/src/Routing/D8RouteRoutes.php
@@ -8,7 +8,7 @@
 namespace Drupal\d8route\Routing;
 
 use Symfony\Component\Routing\Route;
-
+use Symfony\Component\Routing\RouteCollection;
 
 class D8RouteRoutes {
 
@@ -16,16 +16,20 @@ class D8RouteRoutes {
    * {@inheritdoc}
    */
   public function routes() {
-    $routes = array();
-    $routes['d8route.content'] = new Route(
-        '/hello', array(
-      '_content' => '\Drupal\d8route\Controller\D8RouteController::content',
-      '_title' => 'Hello'
-        ), array(
-      '_permission' => 'TRUE',
+    $route_collection = new RouteCollection();
+    $route = new Route(
+        '/hello', 
+        array(
+          '_content' => '\Drupal\d8route\Controller\D8RouteController::content',
+          '_title' => 'Hello'
+        ), 
+        array(
+          '_permission' => 'TRUE',
         )
     );
-    return $routes;
+    $route_collection->add('d8route.content', $route);
+
+    return $route_collection;
   }
 
 }
---
+++ d8route/src/Routing/RouteSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\d8route\Routing\RouteSubscriber.
+ */
+
+namespace Drupal\d8route\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    // Change path '/hello' to '/hello-alter'.
+    if ($route = $collection->get('d8route.content')) {
+      $route->setPath('/hello-alter');
+    }
+  }
+
+}
